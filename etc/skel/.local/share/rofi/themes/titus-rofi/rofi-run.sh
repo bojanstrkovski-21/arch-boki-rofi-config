4df29f5df708ff093f85c701f68da1158533b1a0
bojanstrkovski-21 <bojanstrkovski.21@gmail.com>
2025-05-01 18:27:26 +0200

    rofi configs first

---
+++ etc/skel/.local/share/rofi/themes/titus-rofi/rofi-run.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/bash
+
+# rofi -modes 'run,drun'  -show drun -show-icons -theme /home/bojanstrko/Templates/debian/christitustech/rofi/themes/sidetab-nord.rasi
+
+# rofi -modes 'run,drun'  -show drun -show-icons -theme /home/bojanstrko/.local/share/rofi/themes/titus-rofi/themes/sidetab-nord.rasi
+
+# rofi -modes 'run,drun'  -show drun -show-icons -theme /home/bojanstrko/.local/share/rofi/themes/titus-rofi/themes/nord.rasi
+
+rofi -modes 'run,drun'  -show drun -show-icons -theme /home/bojanstrko/.local/share/rofi/themes/titus-rofi/config.rasi
