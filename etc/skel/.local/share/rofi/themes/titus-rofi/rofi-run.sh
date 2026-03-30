da638221de85ab2037a119ccca6bf1fde93d6891
bojanstrkovski-21 <bojanstrkovski.21@gmail.com>
2026-03-30 18:51:33 +0200

    fixed copying issues

--- etc/skel/.local/share/rofi/themes/titus-rofi/rofi-run.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/bash
-
-# rofi -modes 'run,drun'  -show drun -show-icons -theme /home/bojanstrko/Templates/debian/christitustech/rofi/themes/sidetab-nord.rasi
-
-# rofi -modes 'run,drun'  -show drun -show-icons -theme /home/bojanstrko/.local/share/rofi/themes/titus-rofi/themes/sidetab-nord.rasi
-
-# rofi -modes 'run,drun'  -show drun -show-icons -theme /home/bojanstrko/.local/share/rofi/themes/titus-rofi/themes/nord.rasi
-
-rofi -modes 'run,drun'  -show drun -show-icons -theme /home/bojanstrko/.local/share/rofi/themes/titus-rofi/config.rasi
